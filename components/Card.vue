7f59051e0780554c98db04ee753bf8538da258af
lifeLikeCode <562681041@qq.com>
2019-12-16 17:50:17 +0800

    初始化uniapp项目目录结构

---
+++ components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view class="card-wrapper">
+			<slot></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped>
+	.card-wrapper{
+		background-color: #36C892;
+		width: 326rpx;
+		height: 400rpx;
+		border-radius: 16rpx;
+		border-radius: 16rpx;
+	}
+</style>
