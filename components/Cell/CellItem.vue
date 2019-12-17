8d66a3feaa4809c0c7a5f0c8c6c5f1aa54dc9112
lifeLikeCode <562681041@qq.com>
2019-12-17 20:50:17 +0800

    通知页面开发

---
+++ components/Cell/CellItem.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="cell-wrapper">
+		<view class="cell-">
+			<view class="cell-title">{{title}}</view>
+			<view class="cell-right-content">{{content}}</view>
+			<view class="arrow" v-if="arrow" >
+				<!-- <image src="../../static/icon_list_more@2x.png"></image> -->
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			title:{
+				type:String,
+				default:""
+			},
+			content:{
+				type:String,
+				default:""
+			},
+			arrow:{
+				type:Boolean,
+				default:true
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped lang="stylus">
+	@import url('../../common/mixins.styl')
+	.cell-wrapper {
+		display:flex;
+		
+	}
+.arrow {
+	width:36rpx;
+	height: 36rpx;
+}
+.arrow image {
+	width:36rpx;
+	height: 36rpx;
+}
+</style>
---
+++ pages/notice/notice.vue
@@ -0,0 +1,35 @@
+<template>
+	<!-- <cmd-cell-item title="排班表发布通知(11.25-11.29)" addon="附加文案" ></cmd-cell-item> -->
+	<cell title="排班表发布通知(11.25-11.29)" content="附加文案"></cell>
+</template>
+
+<script>
+	
+	import cell from "@/components/Cell/CellItem.vue"
+	export default {
+		components:{
+			cell
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods:{
+			showNotice() {
+				
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	/deep/.cmd-cell-item-title {
+		font-family: PingFangSC-Regular;
+		font-size: 34rpx;
+		color: #333333;
+		
+		text-align: left;
+		line-height: 1.2;
+	}
+</style>
