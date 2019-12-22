6e26ec735b10e092763974c356ebaeb6c039b067
lifeLikeCode <562681041@qq.com>
2019-12-22 22:41:57 +0800

    通知页面样式修改

--- pages/notice/notice.vue
+++ pages/notice/notice.vue
@@ -1,6 +1,13 @@
 <template>
 	<!-- <cmd-cell-item title="排班表发布通知(11.25-11.29)" addon="附加文案" ></cmd-cell-item> -->
-	<cell title="11.3心肺复苏成功，来自领导的表表表表表表表" content="12:33" :arrow="false"></cell>
+	<cell :arrow="false">
+		<view class="" slot="left-content">
+			11.3心肺复苏成功，来自领导的表表表表表表表
+		</view>
+		<view class="notice-time" slot="right-content">
+			12:33
+		</view>
+	</cell>
 </template>
 
 <script>
@@ -23,5 +30,13 @@
 </script>
 
 <style scoped lang="less">
-	
+	.notice-time {
+		font-family: PingFangSC-Regular;
+		font-size: 12px;
+		color: #999999;
+		letter-spacing: 0;
+		text-align: right;
+		line-height: 18px;
+		margin-right: -36rpx;
+	}
 </style>
