addb40c8b589c80e13b8c2c65d6d7f09418ec18b
胡奇良 <hql@ipms.cn>
2020-02-01 12:49:38 +0800

    Feat:增加定时器 修改页面

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		
+
 	</view>
 </template>
 
@@ -8,15 +8,36 @@
 	export default {
 		data() {
 			return {
-				
+				interval: ''
 			}
 		},
 		methods: {
-			
+			hasUserInfo() {
+				if (uni.getStorageSync('userInfo')) {
+					return true
+				} else {
+					return false
+				}
+			}
+		},
+		onShow() {
+			this.interval = setInterval(() => {
+				console.log(this.hasUserInfo())
+				if (this.hasUserInfo()) {
+					clearInterval(this.interval)
+					uni.redirectTo({
+						url: '../personIndex/personIndex',
+					});
+				}
+			}, 1000)
+
+		},
+		onHide() {
+			clearInterval(this.interval)
 		}
 	}
 </script>
 
 <style scoped>
-	
+
 </style>
